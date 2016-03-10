d211b868be7b6001d64b7b77fb0c5f8c86a939e0
aaruel <wmakit88@gmail.com>
2016-03-10 01:33:51 -0500

    first-commit-a

---
+++ README.md
@@ -0,0 +1,8 @@
+#Basic Android Camera
+
+Who knew android was so fiddly? For everyone who wants to rip their hair out, here is the basic BAREBONES android camera implementation for your downloading pleasure.
+
+Features:
+- Uses camera
+- Shows preview
+- Doesn't explode
---
+++ app/src/main/java/com/aaruel/aaronruel/seccam/Seccam.java
@@ -0,0 +1,66 @@
+package com.aaruel.aaronruel.seccam;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class Seccam extends Activity {
+    private static final String TAG = "CamTestActivity";
+    CameraPreview preview;
+    Button buttonClick;
+    Camera camera;
+    Activity act;
+    Context ctx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ctx = this;
+        act = this;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_seccam);
+
+        preview = new CameraPreview(this, (SurfaceView)findViewById(R.id.surfaceView));
+        preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        ((FrameLayout) findViewById(R.id.layout)).addView(preview);
+        preview.setKeepScreenOn(true);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int numCams = Camera.getNumberOfCameras();
+        if(numCams > 0){
+            try{
+                camera = Camera.open();
+                camera.startPreview();
+                preview.setCamera(camera);
+            } catch (RuntimeException ex){
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(camera != null) {
+            camera.stopPreview();
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+    private void resetCam() {
+        camera.startPreview();
+        preview.setCamera(camera);
+    }
+}
